e4c984b1d3d37c5053b9366cb79412ce2e28e637
Michael Rueter <michael.rueter.96@gmail.com>
2018-10-04 22:34:44 -0700

    Ready for the project!

---
+++ Scripts/Player_Scripts/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+const SPEED = 70
+
+var moveDir = Vector2(0, 0)
+var state = "normal"
+var facing = "front"
+
+#func _ready():
+#
+#
+#
+#	pass
+
+func _physics_process(delta):
+
+	match state:
+		"normal":
+			normal_controls_loop()
+			normal_movement_loop()
+			normal_animation_loop()
+
+	pass
+
+func normal_controls_loop():
+	
+	
+	
+	pass
+
+func normal_movement_loop():
+	
+	var motion = moveDir.normalized() * SPEED
+	move_and_slide(motion, Vector2(0, 0))
+	
+	pass
+
+func normal_animation_loop():
+	
+	
+	
+	pass
