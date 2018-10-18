5e059a8a032d3fd5814b77601d606fdac7bb8aff
rueterscooter <32781947+rueterscooter@users.noreply.github.com>
2018-10-17 23:40:21 -0700

    Add files via upload

---
+++ Scripts/Game_Scripts/GameLoop.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var player = load("res://Scenes/Player.tscn")
+var roomManager = load("res://Scenes/RoomManager.tscn")
+
+func _ready():
+	
+	player = load("res://Scenes/Player.tscn")
+	roomManager = load("res://Scenes/RoomManager.tscn")
+	
+	instantiate_room()
+	instantiate_player()
+	
+	pass
+
+func instantiate_room():
+	
+	
+	
+	pass
+
+func instantiate_player():
+	
+	
+	
+	pass
